65a329a164c6e7c6e7f131e09ec127ecfc022a0b
Yahyobek <caisophiya@gmail.com>
2025-06-16 13:06:49 +0500

    an_najim

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,46 +1,62 @@
-import { useEffect } from 'react'
 import RegisterBannerImg from '../assets/images/books.world.jpg'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div className="w-full h-screen flex items-center lg:justify-between justify-center">
-        <div className="w-[50%] h-[100%] lg:block hidden">
-          <img src={RegisterBannerImg} alt="register-banner-img" className='w-full h-full object-cover' />
-        </div>
-        <form className='lg:w-[50%] md:w-[60%] sm:w-[70%] w-full sm:h-[82%] h-[100%] flex items-center justify-center'>
-          <div className='xl:w-[60%] sm:w-[90%] w-[100%] h-[100%] bg-white sm:rounded-2xl rounded-none relative overflow-hidden'>
-              <div className='w-[100%] sm:h-[20%] h-[30%] flex items-center justify-center'>
-                <h1 className='text-2xl text-blue-500 font-bold text-center'>Ro'yxatdan o'tish</h1>
-              </div>
-              <div className='w-[100%] sm:h-[80%] h-[70%] bg-[#2970ff] absolute bottom-0 rounded-tl-[5rem] py-12 px-6'>
-                  <div>
-                    <label className='text-white'>Ismingiz:</label>
-                    <br />
-                    <input type="text" placeholder='Abdurahmon' className='w-[100%] h-[35px] bg-white px-2 rounded-[5px] mt-2 outline-none ' />
-                  </div>
-                  <div className='mt-4'>
-                    <label className='text-white'>Familyangiz:</label>
-                    <br />
-                    <input type="text" placeholder='Eshonov' className='w-[100%] h-[35px] bg-white px-2 rounded-[5px] mt-2 outline-none ' />
-                  </div>
-                  <div className='mt-4'>
-                    <label className='text-white'>Telefon raqamingiz:</label>
-                    <br />
-                    <input type="text" value="+998" placeholder='Eshonov' className='w-[100%] h-[35px] bg-white px-2 rounded-[5px] mt-2 outline-none ' />
-                  </div>
-                  <div className='mt-4'>
-                    <label className='text-white'>Parol yarating:</label>
-                    <br />
-                    <input type="password" placeholder='********' className='w-[100%] h-[35px] bg-white px-2 rounded-[5px] mt-2 outline-none ' />
-                  </div>
-                  <div className='mt-6'>
-                    <Link to="/login"><button className='w-[100%] h-[35px] bg-white text-[#2970ff] font-bold rounded-[5px] cursor-pointer'>Ro'yhatdan o'tish</button></Link>
-                  </div>
-                  <p className='mt-5 text-gray-300'>Agar accauntingiz bolsa: <Link to="/login" className='text-black underline'>Tizimga kirish</Link></p>
-              </div>
+    <div className="w-full min-h-screen flex flex-col lg:flex-row">
+      
+      {/* Chap taraf - Rasm */}
+      <div className="hidden lg:block lg:w-1/2 h-[300px] lg:h-full">
+        <img 
+          src={RegisterBannerImg} 
+          alt="register-banner-img" 
+          className="w-full h-full object-cover" 
+        />
+      </div>
+
+      {/* O'ng taraf - Forma */}
+      <form className="w-full lg:w-1/2 flex items-center justify-center bg-gray-100 py-10">
+        <div className="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[75%] xl:w-[60%] bg-white rounded-2xl shadow-md overflow-hidden">
+          
+          {/* Header */}
+          <div className="py-6 px-4 flex justify-center">
+            <h1 className="text-2xl text-blue-500 font-bold text-center">Ro'yxatdan o'tish</h1>
           </div>
-        </form>
+
+          {/* Form Inputs */}
+          <div className="bg-[#2970ff] rounded-tl-[3rem] py-8 px-4 sm:px-6">
+            <div className="mb-4">
+              <label className="text-white block">Ismingiz:</label>
+              <input type="text" placeholder='Abdurahmon' className='w-full h-[40px] bg-white px-3 mt-2 rounded-md outline-none' />
+            </div>
+
+            <div className="mb-4">
+              <label className="text-white block">Familyangiz:</label>
+              <input type="text" placeholder='Eshonov' className='w-full h-[40px] bg-white px-3 mt-2 rounded-md outline-none' />
+            </div>
+
+            <div className="mb-4">
+              <label className="text-white block">Telefon raqamingiz:</label>
+              <input type="text" value="+998" placeholder='Telefon raqam' className='w-full h-[40px] bg-white px-3 mt-2 rounded-md outline-none' />
+            </div>
+
+            <div className="mb-6">
+              <label className="text-white block">Parol yarating:</label>
+              <input type="password" placeholder='********' className='w-full h-[40px] bg-white px-3 mt-2 rounded-md outline-none' />
+            </div>
+
+            <Link to="/login">
+              <button className="w-full h-[40px] bg-white text-[#2970ff] font-bold rounded-md cursor-pointer">
+                Ro'yhatdan o'tish
+              </button>
+            </Link>
+
+            <p className="mt-5 text-gray-300 text-sm text-center">
+              Accauntingiz bormi? <Link to="/login" className="text-white underline">Tizimga kirish</Link>
+            </p>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
