26f1c8359bc2e28eb5b9a1571a232d64471dec1a
Yahyobek <caisophiya@gmail.com>
2025-06-18 20:13:24 +0500

    changed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
     <nav className="fixed z-50 flex items-center justify-center md:px-2 px-0 md:w-[80px] md:h-full md:right-0 md:top-0 md:flex-col w-full h-[70px] left-0 bottom-0 flex-row md:left-auto md:bottom-auto">
       <ul
         id="navbar"
-        className="w-full md:h-[34%] h-full bg-white md:rounded-[10px] rounded-0 justify-center text-center md:py-2 py-0 md:px-2 px-0 shadow-xl flex md:flex-col flex-row items-center"
+        className="w-full md:h-[34%] h-full sm:bg-white bg-blue-500 md:rounded-[10px] rounded-0 justify-center text-center md:py-2 py-0 md:px-2 px-0 shadow-xl flex md:flex-col flex-row items-center"
       >
-        <NavLink to="/home" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-blue-500"><House className="text-blue-500" id="icon" /></NavLink>
-        <NavLink to="/search" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-blue-500"><Search className="text-blue-500" id="icon" /></NavLink>
-        <NavLink to="/cart" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-blue-500"><ShoppingCart className="text-blue-500" id="icon" /></NavLink>
-        <NavLink to="/like" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-blue-500"><Heart className="text-blue-500" id="icon" /></NavLink>
-        <NavLink to="/profile" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-blue-500"><User className="text-blue-500" id="icon" /></NavLink>
+        <NavLink to="/home" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><House className="text-white" id="icon" /></NavLink>
+        <NavLink to="/search" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><Search className="text-white" id="icon" /></NavLink>
+        <NavLink to="/cart" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><ShoppingCart className="text-white" id="icon" /></NavLink>
+        <NavLink to="/like" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><Heart className="text-white" id="icon" /></NavLink>
+        <NavLink to="/profile" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><User className="text-white" id="icon" /></NavLink>
       </ul>
     </nav>
   );
