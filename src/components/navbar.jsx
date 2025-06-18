c9d009fbb28ecb148fc8fdc5f60dd0130182ea25
Yahyobek <caisophiya@gmail.com>
2025-06-18 20:15:05 +0500

    changed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,10 +4,10 @@ import { House, Search, ShoppingCart, Heart, User } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <nav className="fixed z-50 flex items-center justify-center md:px-2 px-0 md:w-[80px] md:h-full md:right-0 md:top-0 md:flex-col w-full h-[70px] left-0 bottom-0 flex-row md:left-auto md:bottom-auto">
+    <nav className="fixed z-50 flex items-center justify-center md:px-2 px-0 md:w-[80px] md:h-full md:right-0 md:top-0 md:flex-col w-full sm:h-[70px] h-[100px] left-0 bottom-0 flex-row md:left-auto md:bottom-auto">
       <ul
         id="navbar"
-        className="w-full md:h-[34%] h-full sm:bg-white bg-blue-500 md:rounded-[10px] rounded-0 justify-center text-center md:py-2 py-0 md:px-2 px-0 shadow-xl flex md:flex-col flex-row items-center"
+        className="w-full md:h-[34%] h-full sm:bg-white bg-blue-500 md:rounded-[10px] sm:rounded-0 rounded-tl-[30px] rounded-tr-[30px] justify-center text-center md:py-2 py-0 md:px-2 px-0 shadow-xl flex md:flex-col flex-row items-center"
       >
         <NavLink to="/home" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><House className="text-white" id="icon" /></NavLink>
         <NavLink to="/search" className="flex items-center justify-center sm:py-2 py-4 sm:px-2 px-4 rounded-[10px] mx-2 md:mx-0 mt-0 md:mt-2 border-[1px] border-white"><Search className="text-white" id="icon" /></NavLink>
